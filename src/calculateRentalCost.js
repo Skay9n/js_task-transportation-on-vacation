c92502dc6ea6beae87622c38755f15d12f4d2763
Bohdan Petliuk <body.prosto@gmail.com>
2026-01-31 23:39:27 +0200

    solution_update

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,14 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const total = days * 40;
+  const dailyRate = 40;
+  const off50 = 50;
+  const off20 = 20;
+  const total = days * dailyRate;
 
   if (days > 6) {
-    return total - 50;
+    return total - off50;
   }
 
   if (days > 2) {
-    return total - 20;
+    return total - off20;
   }
 
   return total;
